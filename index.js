5e0ea883a90983e55b5f34ef34fa13d195b6b162
Christian <chrmon2@nilesk12.org>
2021-09-09 16:29:27 -0500

    less

--- index.js
+++ index.js
@@ -32,13 +32,6 @@ require('./config/passport')(passport)
 
 connectDB()
 
-//Init App
-const app = express()
-
-// App Local functions
-
-app.locals.moment = require('moment');
-
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
