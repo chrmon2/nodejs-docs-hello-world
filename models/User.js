82c8000b9429307ed6779d4f82071e9bf361b0b2
Christian <chrmon2@nilesk12.org>
2021-09-10 14:16:25 -0500

    user

--- models/User.js
+++ models/User.js
@@ -45,4 +45,6 @@ const UserSchema = new mongoose.Schema({
     }
 })
 
-module.exports = mongoose.model('User', UserSchema)
+const model = mongoose.model('User', UserSchema)
+
+module.exports = model
