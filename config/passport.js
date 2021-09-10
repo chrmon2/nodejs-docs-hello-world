84d50c0c3f212e631bb3518d0ed93ab6d3895b7b
Christian <chrmon2@nilesk12.org>
2021-09-10 14:25:02 -0500

    nouser

--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,7 @@ const TwitterStrategy = require('passport-twitter').Strategy
 const FacebookStrategy = require('passport-facebook').Strategy
 const LocalStrategy = require('passport-local').Strategy
 const mongoose = require('mongoose')
-const User = require('../models/User')
+//const User = require('../models/User')
 const bcrypt = require('bcrypt')
 
 
