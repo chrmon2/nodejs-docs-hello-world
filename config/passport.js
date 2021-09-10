e9878e82c1b72c337efad3c93cfc82d46331ad18
Christian <chrmon2@nilesk12.org>
2021-09-10 14:40:54 -0500

    no bycrypt

--- config/passport.js
+++ config/passport.js
@@ -3,8 +3,8 @@ const TwitterStrategy = require('passport-twitter').Strategy
 const FacebookStrategy = require('passport-facebook').Strategy
 const LocalStrategy = require('passport-local').Strategy
 const mongoose = require('mongoose')
-const User = require('../models/User')*/
-const bcrypt = require('bcrypt')
+const User = require('../models/User')
+const bcrypt = require('bcrypt')*/
 
 
 const passport = function(passport) {
