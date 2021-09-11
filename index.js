068f71a0be78fd689670ca14df08290f0b3a9e8b
Christian <chrmon2@nilesk12.org>
2021-09-10 20:49:35 -0500

    pages

--- index.js
+++ index.js
@@ -71,6 +71,19 @@ app.use(passport.session())
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/recipes', require('./routes/recipes'))
+
+//404 Page
+app.use(function(req,res){
+    res.status(404).render('layouts/main', {
+        body: 'error/404',
+        user: req.user
+    })
+});
+
 const server = http.createServer(app);
 
 const port = process.env.PORT || 1337;
